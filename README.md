6502b1ba964c1aec42d1664e171e1288f558d8fb
dignifiedquire <dignifiedquire@gmail.com>
2016-03-23 22:11:36 +0100

    refactor: use aegir

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Kademlia Record Store JavaScript Implementation
 [![](https://img.shields.io/badge/project-IPFS-blue.svg?style=flat-square)](http://ipfs.io/)
 [![](https://img.shields.io/badge/freenode-%23ipfs-blue.svg?style=flat-square)](http://webchat.freenode.net/?channels=%23ipfs)
 ![](https://img.shields.io/badge/status-WIP-red.svg?style=flat-square)
+[![Coverage Status](https://coveralls.io/repos/github/diasdavid/js-libp2p-kad-record-store/badge.svg?branch=master)](https://coveralls.io/github/diasdavid/js-libp2p-kad-record-store?branch=master)
+[![Travis CI](https://travis-ci.org/diasdavid/js-libp2p-kad-record-store.svg?branch=master)](https://travis-ci.org/diasdavid/js-libp2p-kad-record-store)
+[![Circle CI](https://circleci.com/gh/diasdavid/js-libp2p-kad-record-store.svg?style=svg)](https://circleci.com/gh/diasdavid/js-libp2p-kad-record-store)
+[![Dependency Status](https://david-dm.org/diasdavid/js-libp2p-kad-record-store.svg?style=flat-square)](https://david-dm.org/diasdavid/js-libp2p-kad-record-store) [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square)](https://github.com/feross/standard)
 
 > JavaScript implementation of IPFS Kademlia Record Store
 
---
+++ test/index.spec.js
@@ -0,0 +1,76 @@
+/* eslint-env mocha */
+'use strict'
+
+var tape = require('tape')
+var tests = require('abstract-record-store/tests')
+var KadRS = require('../src')
+var MerkleDAGStore = require('merkledag-store')
+var multiaddr = require('multiaddr')
+var Id = require('peer-id')
+var Peer = require('peer-info')
+var Swarm = require('libp2p-swarm')
+var KadRouter = require('libp2p-kad-routing')
+var tcp = require('libp2p-tcp')
+var Spdy = require('libp2p-spdy')
+
+describe('kad-record-store', function () {
+  this.timeout(60000)
+
+  it('follows the abstract record spec', (done) => {
+    var common = {
+      setup: function (t, cb) {
+        // Set up two peers full peer, pass one of the kadRS to the tests
+
+        var mh1 = multiaddr('/ip4/127.0.0.1/tcp/8081')
+        var p1 = new Peer(Id.create(), [])
+        var sw1 = new Swarm(p1)
+        sw1.addTransport('tcp', tcp, { multiaddr: mh1 }, {}, {port: 8081}, ready)
+
+        var mh2 = multiaddr('/ip4/127.0.0.1/tcp/8082')
+        var p2 = new Peer(Id.create(), [])
+        var sw2 = new Swarm(p2)
+        sw2.addTransport('tcp', tcp, { multiaddr: mh2 }, {}, {port: 8082}, ready)
+
+        var counter = 0
+
+        function ready () {
+          counter++
+          if (counter < 2) {
+            return
+          }
+          sw1.addStreamMuxer('spdy', Spdy, {})
+          sw1.enableIdentify()
+          sw2.addStreamMuxer('spdy', Spdy, {})
+          sw2.enableIdentify()
+
+          var krOne = new KadRouter(p1, sw1)
+          var krTwo = new KadRouter(p2, sw2)
+
+          krOne.addPeer(p2)
+          krTwo.addPeer(p1)
+
+          var mdagStoreA = new MerkleDAGStore()
+          var krsA = new KadRS(mdagStoreA, {
+            swarm: sw1,
+            kadRouter: krOne
+          })
+
+          var mdagStoreB = new MerkleDAGStore()
+          // eslint-disable-next-line no-unused-vars
+          var krsB = new KadRS(mdagStoreB, {
+            swarm: sw2,
+            kadRouter: krTwo
+          })
+
+          cb(null, krsA)
+        }
+      },
+      teardown: function (t, cb) {
+        done()
+        cb()
+      }
+    }
+
+    tests(tape, common)
+  })
+})
